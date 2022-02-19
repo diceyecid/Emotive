b981c5acbc9ccdcf8b90dfd61450a31302e22639
chl49 <chl49@sfu.ca>
2022-02-18 21:33:39 -0800

    text entry back to menu cycle complete

--- app/src/main/java/com/example/emotive/TextEntryActivity.kt
+++ app/src/main/java/com/example/emotive/TextEntryActivity.kt
@@ -3,12 +3,34 @@ package com.example.emotive
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent.KEYCODE_ENTER
 import kotlinx.android.synthetic.main.activity_text_entry.*
 
 class TextEntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_entry)
+        ////THIS IS SO THAT ON CLICK THE TEXT BOX CLEARS. THE PROBLEM IS THE KEYBOARD STAYS AFTER ENTER
+        ////WE MAY HAVE TO HAVE THE INPUT TEXT AND QUESTION TEXT SEPARATE
+        /*
+        inputText.setOnClickListener{
+            if (inputText.getText().toString() == "Anything You Want To Share?"){
+                inputText.getText().clear()
+            }
+        }
+        */
+
+        ///THIS DIDN'T WORK BUT I'M ARCHIVING IT
+        /*
+        inputText.setOnKeyListener { view, i, keyEvent ->
+            val myText = inputText.text
+            false
+        }
+         */
+
+
+
+
 
         val newFace = intent.getStringExtra("face")
 
