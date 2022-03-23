826788ef5ec71d06e31c7e228d2d5080fdf7eaf4
chl49 <chl49@sfu.ca>
2022-03-22 17:37:57 -0700

    Reward data class

--- app/src/main/java/com/example/emotive/Reward.kt
+++ app/src/main/java/com/example/emotive/Reward.kt
@@ -1,9 +1,16 @@
 package com.example.emotive
 
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
-
+import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity( tableName = "Reward" )
-data class Reward {
+data class Reward(@ColumnInfo(name ="text") var text: String,
+                  @ColumnInfo(name = "petal") var petal: Int,
+                  @ColumnInfo(name = "isNew") var isNew: Boolean
+) : Serializable
+{
+    @PrimaryKey(autoGenerate = true) var uid: Long = 0
 }
