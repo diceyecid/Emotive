214089b7cd18c032e03cefd4e17051e0a7f0348e
chl49 <chl49@sfu.ca>
2022-03-22 17:34:53 -0700

    Item and Reward null

--- app/src/main/java/com/example/emotive/Item.kt
+++ app/src/main/java/com/example/emotive/Item.kt
@@ -1,9 +1,16 @@
 package com.example.emotive
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity( tableName = "Item" )
-data class Item() {
-
+data class Item(@ColumnInfo(name ="type") var type: Int,
+                @ColumnInfo(name = "price") var price: Int,
+                @ColumnInfo(name = "path") var path: String
+                ) : Serializable
+{
+    @PrimaryKey(autoGenerate = true) var uid: Long = 0
 }
 
