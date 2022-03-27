73ca880dcf51f880ef45f61087a524660f459201
chl49 <chl49@sfu.ca>
2022-03-26 19:27:07 -0700

    data items implemented

--- app/src/main/java/com/example/emotive/ShopActivity.kt
+++ app/src/main/java/com/example/emotive/ShopActivity.kt
@@ -2,16 +2,21 @@ package com.example.emotive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_mood_report.*
 import kotlinx.android.synthetic.main.activity_mood_report.mrPrevImage
 import kotlinx.android.synthetic.main.activity_shop.*
 import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_data_shop.*
+import kotlinx.android.synthetic.main.activity_shop.petalTextView
 
 class ShopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shop)
+        //setContentView(R.layout.activity_shop)
+        setContentView(R.layout.activity_data_shop)
 
         var jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "store.json")
         var gson : Gson = Gson()
@@ -20,6 +25,17 @@ class ShopActivity : AppCompatActivity() {
         //var list = arrayListOf<User>()
 
         var userList = gson.fromJson(jsonFileString, Array<Item>::class.java).asList()
+
+        //NEW
+
+        val recycler = findViewById<RecyclerView>(R.id.itemRecyclerView)
+
+        //setting recycler to horizontal scroll
+        itemRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val samples = loadSampleData()
+
+        //itemRecyclerView.layoutManager = LinearLayoutManager(this)
+        itemRecyclerView.adapter = ItemRecyclerViewAdapter(samples)
 
         /*
         button.setOnClickListener {
@@ -33,5 +49,13 @@ class ShopActivity : AppCompatActivity() {
         mrPrevImage.setOnClickListener {
             finish()
         }
+    }
+    private fun loadSampleData(): ArrayList<Item> {
+        val moodList: ArrayList<Item> = ArrayList<Item>()
+        moodList.add( Item(0, 20,"@drawable/item_button", 0 ) )
+        moodList.add(  Item(0, 20,"@drawable/item_button", 1 ) )
+        moodList.add(  Item(0, 20,"@drawable/item_button", 2 )  )
+
+        return moodList
     }
 }
